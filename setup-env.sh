0e77dcb6bb15ae38c95e1feb192e31e338984de6
Lin Sun <lin.sun@solo.io>
2024-09-09 11:52:09 -0400

    add policies

--- setup-env.sh
+++ setup-env.sh
@@ -4,6 +4,9 @@ k3d cluster create --api-port 6550 -p '9080:80@loadbalancer' -p '9443:443@loadba
 # Install Istio ambient
 istioctl install --set profile=ambient --skip-confirmation  --set values.cni.cniConfDir=/var/lib/rancher/k3s/agent/etc/cni/net.d --set values.cni.cniBinDir=/bin --set meshConfig.accessLogFile=/dev/stdout
 
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml; }
+
 ISTIO_DOWNLOAD_DIR=~/Downloads/istio-1.23.0
 kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/prometheus.yaml
 kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/kiali.yaml
