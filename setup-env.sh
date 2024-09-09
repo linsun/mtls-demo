a7a49e51eefcd0b6e9a94f1b7499ee892d7b68ec
Lin Sun <lin.sun@solo.io>
2024-09-09 11:36:49 -0400

    simple client name

--- setup-env.sh
+++ setup-env.sh
@@ -3,10 +3,7 @@ k3d cluster create --api-port 6550 -p '9080:80@loadbalancer' -p '9443:443@loadba
 
 # Install Istio ambient
 istioctl install --set profile=ambient --skip-confirmation  --set values.cni.cniConfDir=/var/lib/rancher/k3s/agent/etc/cni/net.d --set values.cni.cniBinDir=/bin --set meshConfig.accessLogFile=/dev/stdout
-kubectl create namespace monitoring
-kubectl apply -f ~/istio-1.22.0/prometheus.yaml
-kubectl apply -f ~/istio-1.22.0/kiali.yaml
 
-kubectl create namespace monitoring
-kubectl apply -f ~/istio-1.22.0/prometheus.yaml
-kubectl apply -f ~/istio-1.22.0/kiali.yaml
+ISTIO_DOWNLOAD_DIR=~/Downloads/istio-1.23.0
+kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/prometheus.yaml
+kubectl apply -f $ISTIO_DOWNLOAD_DIR/samples/addons/kiali.yaml
